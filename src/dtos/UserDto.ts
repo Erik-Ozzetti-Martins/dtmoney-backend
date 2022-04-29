bc7b00364aa26bc488d84bd8e86d70967f14ce3d
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-04-28 22:28:01 -0300

    dtos

---
+++ src/dtos/UserDto.ts
@@ -0,0 +1,12 @@
+import { User } from 'entities/User';
+export class UserDto {
+  id: string;
+  name: string;
+  email: string;
+
+  constructor(user: User) {
+    this.id = user.id;
+    this.name = user.name;
+    this.email = user.email;
+  }
+}
