5f099832d06862d9cf75a0770084b85a176952b2
erikmartins <erik.ozzetti.martins@gmail.com>
2022-12-28 01:41:06 -0300

    chore: files

--- src/modules/auth/useCases/authSignIn/AuthSignIn.Controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export class AuthSignInController {
-  execute(request: Request, response: Response): Response {
-    return response.json({ message: 'Hello World' });
-  }
-}
