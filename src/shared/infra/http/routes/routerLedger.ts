b8981e7e30f62eb2ba84182b027ce12488230e35
erikmartins <erik.ozzetti.martins@gmail.com>
2022-12-28 02:44:53 -0300

    feat: routes ledger

--- src/shared/infra/http/routes/routerLedger.ts
+++ src/shared/infra/http/routes/routerLedger.ts
@@ -1,11 +1,13 @@
-import { LedgerController } from 'controller/Ledger.Controller';
+import { ensureAuthenticated } from '@/shared/infra/http/middlewares/ensureAuthenticated';
+import { CreateLedgerController } from '@/modules/ledger/useCases/CreateLedger/CreateLedger.Controller';
+import { FindLeadgerController } from '@/modules/ledger/useCases/FindLedger/FindLeadger.Controller';
 import { Router } from 'express';
-import { ensureAuthenticated } from 'middlewares/ensureAuthenticated';
 
 const routerLedger = Router();
-const ledgerController = new LedgerController();
+const createLedgerController = new CreateLedgerController();
+const findLeadgerController = new FindLeadgerController();
 
-routerLedger.post('/', ensureAuthenticated, ledgerController.created);
-routerLedger.get('/', ensureAuthenticated, ledgerController.getAll);
+routerLedger.post('/', ensureAuthenticated, createLedgerController.execute);
+routerLedger.get('/', ensureAuthenticated, findLeadgerController.execute);
 
 export { routerLedger };
