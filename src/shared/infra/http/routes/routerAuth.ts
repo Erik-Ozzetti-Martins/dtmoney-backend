55494ab0e6c08b792903dd424e31fa222329a8ce
erikmartins <erik.ozzetti.martins@gmail.com>
2022-12-28 02:44:31 -0300

    feat: routes auth

--- src/shared/infra/http/routes/routerAuth.ts
+++ src/shared/infra/http/routes/routerAuth.ts
@@ -1,13 +1,23 @@
-import AuthController from 'controller/Auth.Controller';
 import { Router } from 'express';
+import { SignInController } from '@/modules/auth/useCases/SignIn/SignIn.Controller';
+import { SignUpController } from '@/modules/auth/useCases/SignUp/SignUp.Controller';
+import { SendForgotPasswordMailController } from '@/modules/auth/useCases/SendForgotPasswordMail/SendForgotPasswordMail.Controller';
+import { RefreshTokenController } from '@/modules/auth/useCases/RefreshToken/RefreshToken.Controller';
+import { ResetPasswordController } from '@/modules/auth/useCases/ResetPassword/ResetPassword.Controller';
 
 const routerAuth = Router();
-const controllerAuth = new AuthController();
+const signIn = new SignInController();
+const signUp = new SignUpController();
+const sendForgotPasswordMail = new SendForgotPasswordMailController();
+const refreshToken = new RefreshTokenController();
+const resetPassword = new ResetPasswordController();
 
-routerAuth.post('/login', controllerAuth.login);
-routerAuth.post('/register', controllerAuth.register);
-routerAuth.post('/refresh-token', controllerAuth.refreshToken);
-routerAuth.post('/forgot', controllerAuth.sendForgotPasswordMail);
-routerAuth.post('/password/reset', controllerAuth.resetPassword);
+routerAuth.post('/signin', signIn.execute);
+routerAuth.post('/signup', signUp.execute);
+
+routerAuth.post('/forgot', sendForgotPasswordMail.execute);
+
+routerAuth.post('/refresh-token', refreshToken.execute);
+routerAuth.post('/password/reset', resetPassword.execute);
 
 export default routerAuth;
