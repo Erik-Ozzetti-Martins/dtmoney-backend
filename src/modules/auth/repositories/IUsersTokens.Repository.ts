c80560962e5477ee9bdb8fbbcdf69dbf4566cca6
erikmartins <erik.ozzetti.martins@gmail.com>
2022-12-28 01:38:19 -0300

    feat:  repository users  and users  repository in memory

--- src/modules/auth/repositories/IUsersTokens.Repository.ts
+++ src/modules/auth/repositories/IUsersTokens.Repository.ts
@@ -1,11 +1,22 @@
-import { ICreateUserTokenDTO } from 'dtos/ICreateUserTokenDTO';
-import { UserTokens } from 'entities/UserTokens';
+import { ICreateUserTokenDTO } from '@/modules/auth/dtos/ICreateUserTokenDTO';
 
 interface IUsersTokensRepository {
   create({
     expires_date,
     refresh_token,
     user_id,
-  }: ICreateUserTokenDTO): Promise<UserTokens>;
+  }: ICreateUserTokenDTO): Promise<ICreateUserTokenDTO>;
+  findByUserToken(refresh_token: string): Promise<ICreateUserTokenDTO>;
+  deleteById(id: string): Promise<void>;
 }
+
 export { IUsersTokensRepository };
+
+export interface IUsersTokensRepositoryD {
+  id: string;
+  user_id: string;
+  refresh_token: string;
+  expires_date: Date;
+  created_at: Date;
+  updated_at: Date;
+}
