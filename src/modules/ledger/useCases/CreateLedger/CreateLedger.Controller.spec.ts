d5e3d393a788a1a30ea92bffbd1a69e6a49711a0
erikmartins <erik.ozzetti.martins@gmail.com>
2022-12-28 02:43:14 -0300

    feat:  createLedger controller  and spec

---
+++ src/modules/ledger/useCases/CreateLedger/CreateLedger.Controller.spec.ts
@@ -0,0 +1,37 @@
+import request from 'supertest';
+import { app } from '@/shared/infra/http/app';
+
+describe('Create Ledger Controller', () => {
+  it('should be able to create a new ledger', async () => {
+    const signIn = await request(app).post('/api/v1/auth/signin').send({
+      email: 'testAA@gmail.com',
+      password: 'testete.com',
+    });
+
+    const response = await request(app)
+      .post('/api/v1/ledger/')
+      .set('Authorization', `Bearer ${signIn.body.token} `)
+      .send({
+        amount: 22.0,
+        category: 'teste',
+        title: 'teste',
+        type: 'gasto',
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('should not be able to create a new ledger with invalid token', async () => {
+    const response = await request(app)
+      .post('/api/v1/ledger/')
+      .set('Authorization', 'Bearer 1234567890')
+      .send({
+        amount: 22.0,
+        category: 'teste',
+        title: 'teste',
+        type: 'gasto',
+      });
+
+    expect(response.status).toBe(401);
+  });
+});
