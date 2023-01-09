04508a350da4e227f51aa57908dbd0965c124fda
erikmartins <erik.ozzetti.martins@gmail.com>
2023-01-09 16:39:55 -0300

    feat: router auth

--- src/shared/infra/http/routes/routerAuth.ts
+++ src/shared/infra/http/routes/routerAuth.ts
@@ -4,6 +4,7 @@ import { SignUpController } from '@/modules/auth/useCases/SignUp/SignUp.Controll
 import { SendForgotPasswordMailController } from '@/modules/auth/useCases/SendForgotPasswordMail/SendForgotPasswordMail.Controller';
 import { RefreshTokenController } from '@/modules/auth/useCases/RefreshToken/RefreshToken.Controller';
 import { ResetPasswordController } from '@/modules/auth/useCases/ResetPassword/ResetPassword.Controller';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const routerAuth = Router();
 const signIn = new SignInController();
@@ -12,12 +13,58 @@ const sendForgotPasswordMail = new SendForgotPasswordMailController();
 const refreshToken = new RefreshTokenController();
 const resetPassword = new ResetPasswordController();
 
-routerAuth.post('/signin', signIn.execute);
-routerAuth.post('/signup', signUp.execute);
+routerAuth.post(
+  '/signin',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  signIn.execute,
+);
+routerAuth.post(
+  '/signup',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  signUp.execute,
+);
 
-routerAuth.post('/forgot', sendForgotPasswordMail.execute);
+routerAuth.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
 
-routerAuth.post('/refresh-token', refreshToken.execute);
-routerAuth.post('/password/reset', resetPassword.execute);
+  sendForgotPasswordMail.execute,
+);
+
+routerAuth.post(
+  '/refresh-token',
+  celebrate({
+    [Segments.BODY]: {
+      refresh_token: Joi.string().uuid().required(),
+    },
+  }),
+  refreshToken.execute,
+);
+routerAuth.post(
+  '/password/reset',
+
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  resetPassword.execute,
+);
 
 export default routerAuth;
