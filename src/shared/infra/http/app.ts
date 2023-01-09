f441f3a53d77324797dae8891dca1d3eddba5c74
erikmartins <erik.ozzetti.martins@gmail.com>
2023-01-09 16:38:25 -0300

    feat:  configs

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,14 +4,19 @@ import '../../container';
 import 'express-async-errors';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
+import rateLimiter from './middlewares/rateLimiter';
 import AppError from '../../errors/AppError';
 import indexRouter from './routes/routerIndex';
 import swaggerFile from '../../../swagger.json';
 import { config } from 'dotenv';
+import { CelebrateError, errors } from 'celebrate';
+
 config();
 const app = express();
 app.use(express.json());
+app.use(errors());
 app.use(cors());
+app.use(rateLimiter);
 app.use(indexRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -24,6 +29,14 @@ app.use(
         message: error.message,
       });
     }
+
+    if (error instanceof CelebrateError) {
+      const errorBody = error.details.get('body');
+      return response.status(400).json({
+        message: errorBody.message.split('"').join(''),
+      });
+    }
+
     return response.status(500).json({
       status: 'error',
       message: 'internal error',
