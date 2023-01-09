7be9810f7025e97ecb7ab7cc09f9f03666863114
erikmartins <erik.ozzetti.martins@gmail.com>
2023-01-09 16:40:46 -0300

    feat: router ledger

--- src/shared/infra/http/routes/routerLedger.ts
+++ src/shared/infra/http/routes/routerLedger.ts
@@ -2,12 +2,25 @@ import { ensureAuthenticated } from '@/shared/infra/http/middlewares/ensureAuthe
 import { CreateLedgerController } from '@/modules/ledger/useCases/CreateLedger/CreateLedger.Controller';
 import { FindLeadgerController } from '@/modules/ledger/useCases/FindLedger/FindLeadger.Controller';
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const routerLedger = Router();
 const createLedgerController = new CreateLedgerController();
 const findLeadgerController = new FindLeadgerController();
 
-routerLedger.post('/', ensureAuthenticated, createLedgerController.execute);
+routerLedger.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      type: Joi.string().required(),
+      title: Joi.string().required(),
+      amount: Joi.number().required(),
+      category: Joi.string().required(),
+    },
+  }),
+  ensureAuthenticated,
+  createLedgerController.execute,
+);
 routerLedger.get('/', ensureAuthenticated, findLeadgerController.execute);
 
 export { routerLedger };
